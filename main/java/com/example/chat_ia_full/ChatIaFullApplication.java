68d1e02ea9088375fb894e026c37f95a86b200f1
Pedro B. Castro <99338512+PBelle451@users.noreply.github.com>
2025-10-16 17:21:46 -0300

    Add files via upload

---
+++ main/java/com/example/chat_ia_full/ChatIaFullApplication.java
@@ -0,0 +1,13 @@
+package com.example.chat_ia_full;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatIaFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatIaFullApplication.class, args);
+	}
+
+}
