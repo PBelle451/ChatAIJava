2a1393ac76fb72bf18bcc98debbf91a3751082d6
Pedro B. Castro <99338512+PBelle451@users.noreply.github.com>
2025-10-16 17:23:53 -0300

    Add files via upload

---
+++ chat-ia-full/README.md
@@ -0,0 +1,3 @@
+# ChatAIJava
+# ChatAIJava
+# ChatAIJava
---
+++ chat-ia-full/src/test/java/com/example/chat_ia_full/ChatIaFullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chat_ia_full;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatIaFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
